054cbe424deb81cc8b0165aedf3ba64920263626
Stelter <7280681@linux01.mcs.sdsmt.edu>
2016-11-02 14:18:23 -0600

    Fixed cuda functions

--- cudaPrime.cu
+++ cudaPrime.cu
@@ -11,26 +11,26 @@
 using namespace std;
 
 //test for a numbers primality
-__global__ void isPrimeCoarse( ull offset, ull start, ull end, int * result )
+__global__ void isPrimeCoarse( ull offset, ull start, ull end, int* result )
 {
 	ull tid = threadIdx.x + blockIdx.x * blockDim.x;
 	ull idx = tid + offset;
 	ull val = tid + start;
 
-    int prime = 1;
+    char prime = 0;
     if(val <= end)
     {
 	    if( val < 2 )
 	    {
-		    prime = 0;
+		    prime = 1;
 	    }
         else
         {
-	        for( int i = 2; i < val / 2; i++ )
+	        for( ull i = 2; i < val / 2; i++ )
 	        {
 		        if( val % i == 0 )
 		        {
-			        prime = 0;
+			        prime = 1;
 		        }
 	        }
 	    }
@@ -42,33 +42,45 @@ __global__ void isPrimeCoarse( ull offset, ull start, ull end, int * result )
 //test for a numbers primality
 __global__ void isPrimeFine( ull val, ull offset, int * result )
 {
-    //+2 to ignore 0, and 1
     bool prime = true;
-    ull tid = threadIdx.x + blockIdx.x * blockDim.x + offset;
-    if(tid == 0) *result = 1;
-    if(val < 1) prime = false;
-    else if(tid > 1 && tid < val)
+    ull tid = threadIdx.x + blockIdx.x * blockDim.x + offset + 2;
+
+    if(val < 2)
+    {
+        if(tid == 2) *result = 1;
+    }
+    else if(tid < val/2)
     {
         if(val % tid == 0)
         {
             prime = false;
         }
+    
+        if(!prime) *result = 1;
     }
-    if(tid == 0)
-        if(__any(!prime)) *result = 0;
 }
 
 //test for a numbers primality
 __global__ void isPrimeHybrid( ull store_offset, ull val_offset, int * result )
 {
-    ull tid = threadIdx.x;
+    ull tid = threadIdx.x+2;
     ull val = blockIdx.x + val_offset;
     ull idx = blockIdx.x + store_offset;
-
-    if(tid == 0)result[idx] = 1;
-    for(ull i=tid; i<val; i+=blockDim.x)
+    ull lim = val/2;
+    bool prime = true;
+    
+    if(val < 2)
     {
-        if(val % i == 0) result[idx] = 0;
+        if(tid == 2)result[idx] = 1;
+    }
+    else
+    {
+        for(ull i=tid; i<lim; i+=blockDim.x)
+        {
+            if(val % i == 0) prime = false;
+        }
+
+        if(!prime) result[idx] = 1;
     }
 }
 
@@ -107,7 +119,7 @@ string getCudaDeviceProperties()
     return out;
 }
 
-ull sumRange(int * data, ull size, int warps)
+ull sumRange(int* data, ull size, int warps)
 {   
     //cout << "\nStarting cuda range sum on " << size << " items" << endl;
 
@@ -181,7 +193,8 @@ int runCudaCoarse( ull start, ull end, unsigned int warps )
 	    cout << "Error allocating memory on cuda device" << endl;
 	    return 0;
 	}
-	int *result = (int *)malloc( size );
+	
+    cudaMemset(d_result, 0, size);
 	
     ull threadsThisTime;
     ull totalDone=0;
@@ -205,14 +218,12 @@ int runCudaCoarse( ull start, ull end, unsigned int warps )
     {
         cout << "runCudaCoarse::Error during kernel execution: " << cudaGetErrorString(lastError) << endl;
         cudaFree( d_result );
-	    free( result );
         return 0;
     }
     
-    int count = sumRange(d_result, range, warps);
+    int count = range-sumRange(d_result, range, warps);
 
 	cudaFree( d_result );
-	free( result );
 	
 	return count;
 }
@@ -232,8 +243,8 @@ int runCudaFine( ull start, ull end, unsigned int warps )
 	    cout << "Error allocating memory on cuda device" << endl;
 	    return 0;
 	}
-    int* result = (int *)malloc( size );
-        
+    cudaMemset(d_result, 0, size);
+           
     ull blocks;
     //i - index of return location
     //j - value to test
@@ -241,11 +252,12 @@ int runCudaFine( ull start, ull end, unsigned int warps )
     for(ull j=start; j<=end; i++, j++)
     {
         //k - Offset from 0 for thread indices
-        for(ull k=0; k<j; k+= threadsPer*MAXBLOCKS)
+        for(ull k=0; k<=j; k+= threadsPer*MAXBLOCKS)
         {
-            blocks = (ull)(ceil((double)j/threadsPer)+0.2);
+            blocks = (ull)(ceil((double)(j/2)/threadsPer)+0.2);
             if(blocks > MAXBLOCKS) blocks = MAXBLOCKS;
-            
+            if(!blocks) blocks = 1;
+
             //cout << "IsPrimeFine<<<" << blocks << ", " << threadsPer << ">>>(" << j << ", " << k << ", " << i << ")" << endl;
             isPrimeFine<<<blocks, threadsPer>>>( j, k, i );
         }
@@ -257,15 +269,14 @@ int runCudaFine( ull start, ull end, unsigned int warps )
     {
         cout << "runCudaFine::Error during kernel execution: " << cudaGetErrorString(lastError) << endl;
         cudaFree( d_result );
-	    free( result );
+	    
         return 0;
     }
     
-    int count = sumRange(d_result, range, warps);
+    int count = range-sumRange(d_result, range, warps);
     
     cudaFree( d_result );
-    free( result );
-    
+     
     return count;
 }
 
@@ -284,7 +295,7 @@ int runCudaHybrid( ull start, ull end, unsigned int warps )
 	    cout << "Error allocating memory on cuda device" << endl;
 	    return 0;
 	}
-    int* result = (int *)malloc( size );
+    cudaMemset(d_result, 0, size);
     
     ull blocks;
     for(ull i=start, j=0; i<end; i+= MAXBLOCKS, j+=MAXBLOCKS)
@@ -302,13 +313,12 @@ int runCudaHybrid( ull start, ull end, unsigned int warps )
     {
         cout << "runCudaHybrid::Error during kernel execution: " << cudaGetErrorString(lastError) << endl;
         cudaFree( d_result );
-	    free( result );
+	    
         return 0;
     }
-    int count = sumRange(d_result, range, warps);
+    int count = range-sumRange(d_result, range, warps);
     
     cudaFree( d_result );
-    free( result );
-    
+       
     return count;
 }
